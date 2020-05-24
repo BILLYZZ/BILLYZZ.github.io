c0f652c9ab6aad827275d1651a1192b26c7b9a24
BILLYZZ <alphabilly@berkeley.edu>
2020-05-24 15:17:32 -0700

    wp-includes

---
+++ wp-admin/moderation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://billyzz.github.io/wp-admin/edit-comments.php?comment_status=moderated">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://billyzz.github.io/wp-admin/edit-comments.php?comment_status=moderated";
+		</script>
+
+		<p>You are being redirected to <a href="http://billyzz.github.io/wp-admin/edit-comments.php?comment_status=moderated">http://billyzz.github.io/wp-admin/edit-comments.php?comment_status=moderated</a></p>
+	</body>
+</html>
---
+++ wp-links-opml.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<opml version="1.0">
+	<head>
+		<title>
+		Links for testsite		</title>
+		<dateCreated>Sun, 24 May 2020 22:13:00 GMT</dateCreated>
+		<!-- generator="WordPress/5.2.6" -->
+	</head>
+	<body>
+</body>
+</opml>
